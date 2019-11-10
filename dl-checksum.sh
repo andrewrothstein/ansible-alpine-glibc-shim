38768d9ecd9e3a1268c0f7ad42403e15805474d4
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-09 23:14:46 -0500

    up to 2.30-r0

---
+++ dl-checksum.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+DIR=~/Downloads
+MIRROR=https://github.com/sgerrand/alpine-pkg-glibc/releases/download
+
+dl()
+{
+    local ver=$1
+    local app=$2
+    local file=$app-$ver.apk
+    local url=$MIRROR/$ver/$file
+    local lfile=$DIR/$file
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $app `sha256sum $lfile | awk '{print $1}'`
+}
+
+dlver () {
+    local ver=$1
+    printf "  '%s'\n" $ver
+    dl $ver glibc
+    dl $ver glibc-bin
+    dl $ver glibc-i18n
+}
+
+dlver ${1:-2.30-r0}
